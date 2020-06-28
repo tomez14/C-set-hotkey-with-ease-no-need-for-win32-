ec3b141ea04d4721b5f99d4172969aadf295ecfb
tomez14 <41573504+tomez14@users.noreply.github.com>
2020-06-28 16:48:08 +0300

    Add files via upload

---
+++ Example.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using SimpleHotKeys;
+using System.Windows.Forms;
+namespace Example
+{
+    public class example : HotKey
+    {
+        string text;
+        public example(string text, Keys hotkey, bool toggle) : base(hotkey, toggle)//here I made a constructor which inherits from the base constructor aka part 4 of README, it is neccesery. also remeber to make it public for you to be able to access it
+        {
+            this.text = text;
+        }
+        public override void whenPressed()//you HAVE TO override this function for this class to work, what is inside here will be the code you want to execute once the hotkey was pressed
+        {
+            Console.WriteLine(text);
+        }
+    }
+}
